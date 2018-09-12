6e35f26f544f9351ad40b917658367a28b4e7330
guixiang <gxiang0208@126.com>
2018-09-12 17:32:10 +0800

    activiti+springboot

---
+++ src/main/java/com/xiang/activiti/ActivitiApplication.java
@@ -0,0 +1,15 @@
+package com.xiang.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.activiti.spring.boot.SecurityAutoConfiguration.class})
+public class ActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiApplication.class, args);
+    }
+}
